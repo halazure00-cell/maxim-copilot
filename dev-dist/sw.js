456a282a45e4754a317cb6a74ce78aaa8ce8ed3e
adi.supriyadi.aer24 <adi.supriyadi.aer24@polban.ac.id>
2026-01-29 13:01:06 +0000

    Changes before Firebase Studio auto-run

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-ca84f546'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.tpmbe1d7ljg"
+    "revision": "0.d9pi6n70mu"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
